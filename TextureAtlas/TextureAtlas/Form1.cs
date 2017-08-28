202482b99fc26f0c0c373dc1e4ae761dbf254543
AIE\s171102 <alpi1997@hotmail.com>
2017-08-28 16:28:19 +1000

    selecting sprites draws

--- TextureAtlas/TextureAtlas/Form1.cs
+++ TextureAtlas/TextureAtlas/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
@@ -14,55 +15,76 @@ namespace TextureAtlas
 {
     public partial class Form1 : Form
     {
-        List<Rectangle> asd;
+      //  List<Rectangle> asd;
+
+      //Rectangle mRect;
 
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
+        private Point RectStartPoint;
+        private Rectangle Rect = new Rectangle();
+        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
 
-        }
-        private Point firstPoint = new Point();
-        private int x = 0;
-        private int y = 0;
-
+        // Start Rectangle
+        //
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            x = e.X;
-            y = e.Y;
+            // Determine the initial rectangle coordinates...
+            RectStartPoint = e.Location;
+            pictureBox1.Invalidate();
+            pictureBox1.Refresh();
         }
 
+        // Draw Rectangle
+        //
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+            pictureBox1.Invalidate();
+            pictureBox1.Refresh();
+        }
+
+        // Draw Area
+        //
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Draw the rectangle...
+            if (pictureBox1.Image != null)
             {
-                pictureBox1.Left = (pictureBox1.Left + e.X) - x;
-                pictureBox1.Top = (pictureBox1.Top + e.Y) - y;
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                {
+                    e.Graphics.FillRectangle(selectionBrush, Rect);
+                }
             }
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-
-            if (e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Right)
             {
-                pictureBox1.Left = x;
-                pictureBox1.Top = y;
+                if (Rect.Contains(e.Location))
+                {
+                    Debug.WriteLine("Right click");
+                }
             }
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
         }
 
         private void Save_Click(object sender, EventArgs e)
         {
             SaveFileDialog save = new SaveFileDialog();
-            save.Filter = "Image files|*.png;*.jpg;*.bmp;";
+            save.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;";
 
             string pathDefault = Environment.CurrentDirectory;
             pathDefault = Path.GetFullPath(Path.Combine(pathDefault, @"..\"));
