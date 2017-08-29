8e1c07b57db093dec55d5c7aa6d8909d11572f32
AIE\s171102 <alpi1997@hotmail.com>
2017-08-29 17:21:50 +1000

    hgyg

--- TextureAtlas/TextureAtlas/Form1.Designer.cs
+++ TextureAtlas/TextureAtlas/Form1.Designer.cs
@@ -28,185 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.Load = new System.Windows.Forms.Button();
-            this.Save = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.Load);
-            this.groupBox1.Controls.Add(this.Save);
-            this.groupBox1.Controls.Add(this.button7);
-            this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 593);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // Load
-            // 
-            this.Load.AutoSize = true;
-            this.Load.Dock = System.Windows.Forms.DockStyle.Right;
-            this.Load.Location = new System.Drawing.Point(103, 380);
-            this.Load.Name = "Load";
-            this.Load.Size = new System.Drawing.Size(94, 210);
-            this.Load.TabIndex = 11;
-            this.Load.Text = "Load";
-            this.Load.UseVisualStyleBackColor = true;
-            this.Load.Click += new System.EventHandler(this.Load_Click);
-            // 
-            // Save
-            // 
-            this.Save.AutoSize = true;
-            this.Save.Dock = System.Windows.Forms.DockStyle.Left;
-            this.Save.Location = new System.Drawing.Point(3, 380);
-            this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(94, 210);
-            this.Save.TabIndex = 10;
-            this.Save.Text = "Save";
-            this.Save.UseVisualStyleBackColor = true;
-            this.Save.Click += new System.EventHandler(this.Save_Click);
-            // 
-            // button7
-            // 
-            this.button7.AutoSize = true;
-            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button7.Location = new System.Drawing.Point(3, 328);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(194, 52);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.AutoSize = true;
-            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.Location = new System.Drawing.Point(3, 276);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(194, 52);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.AutoSize = true;
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.Location = new System.Drawing.Point(3, 224);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(194, 52);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.AutoSize = true;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.Location = new System.Drawing.Point(3, 172);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(194, 52);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.AutoSize = true;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.Location = new System.Drawing.Point(3, 120);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(194, 52);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.AutoSize = true;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.Location = new System.Drawing.Point(3, 68);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(194, 52);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.AutoSize = true;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Location = new System.Drawing.Point(3, 16);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 52);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.pictureBox1);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(200, 0);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1252, 593);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1246, 574);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
-            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1452, 593);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.Loader = new System.Windows.Forms.Button();
+			this.Save = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.Delete = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.Loader);
+			this.groupBox1.Controls.Add(this.Save);
+			this.groupBox1.Controls.Add(this.button7);
+			this.groupBox1.Controls.Add(this.button6);
+			this.groupBox1.Controls.Add(this.button5);
+			this.groupBox1.Controls.Add(this.button4);
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.groupBox1.Location = new System.Drawing.Point(0, 0);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(200, 593);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// Loader
+			// 
+			this.Loader.AutoSize = true;
+			this.Loader.Dock = System.Windows.Forms.DockStyle.Right;
+			this.Loader.Location = new System.Drawing.Point(103, 380);
+			this.Loader.Name = "Loader";
+			this.Loader.Size = new System.Drawing.Size(94, 210);
+			this.Loader.TabIndex = 11;
+			this.Loader.Text = "Load";
+			this.Loader.UseVisualStyleBackColor = true;
+			this.Loader.Click += new System.EventHandler(this.Load_Click);
+			// 
+			// Save
+			// 
+			this.Save.AutoSize = true;
+			this.Save.Dock = System.Windows.Forms.DockStyle.Left;
+			this.Save.Location = new System.Drawing.Point(3, 380);
+			this.Save.Name = "Save";
+			this.Save.Size = new System.Drawing.Size(94, 210);
+			this.Save.TabIndex = 10;
+			this.Save.Text = "Save";
+			this.Save.UseVisualStyleBackColor = true;
+			this.Save.Click += new System.EventHandler(this.Save_Click);
+			// 
+			// button7
+			// 
+			this.button7.AutoSize = true;
+			this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button7.Location = new System.Drawing.Point(3, 328);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(194, 52);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.AutoSize = true;
+			this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button6.Location = new System.Drawing.Point(3, 276);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(194, 52);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.AutoSize = true;
+			this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button5.Location = new System.Drawing.Point(3, 224);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(194, 52);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.AutoSize = true;
+			this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button4.Location = new System.Drawing.Point(3, 172);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(194, 52);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.AutoSize = true;
+			this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button3.Location = new System.Drawing.Point(3, 120);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(194, 52);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button2.Location = new System.Drawing.Point(3, 68);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(194, 52);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button1.Location = new System.Drawing.Point(3, 16);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(194, 52);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.pictureBox1);
+			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+			this.groupBox2.Location = new System.Drawing.Point(200, 0);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(1012, 593);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "groupBox2";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1006, 574);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.Delete);
+			this.groupBox3.Controls.Add(this.listBox1);
+			this.groupBox3.Location = new System.Drawing.Point(1215, 0);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(238, 593);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "groupBox3";
+			// 
+			// Delete
+			// 
+			this.Delete.Location = new System.Drawing.Point(6, 540);
+			this.Delete.Name = "Delete";
+			this.Delete.Size = new System.Drawing.Size(226, 53);
+			this.Delete.TabIndex = 1;
+			this.Delete.Text = "Delete";
+			this.Delete.UseVisualStyleBackColor = true;
+			this.Delete.Click += new System.EventHandler(this.Delete_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(6, 16);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(226, 524);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.ClientSize = new System.Drawing.Size(1452, 593);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
 
         }
 
@@ -221,8 +257,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Loader;
         private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Delete;
     }
 }
 
--- TextureAtlas/TextureAtlas/Form1.cs
+++ TextureAtlas/TextureAtlas/Form1.cs
@@ -13,100 +13,127 @@ using System.Drawing.Imaging;
 
 namespace TextureAtlas
 {
-    public partial class Form1 : Form
-    {
-      //  List<Rectangle> asd;
+	public partial class Form1 : Form
+	{
+		List<Rectangle> list = new List<Rectangle>();
 
-      //Rectangle mRect;
+		//Rectangle mRect;
 
-        public Form1()
-        {
-            InitializeComponent();
-            this.DoubleBuffered = true;
-        }
+		public Form1()
+		{
+			InitializeComponent();
+			this.DoubleBuffered = true;
+		}
 
-        private Point RectStartPoint;
-        private Rectangle Rect = new Rectangle();
-        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+		private Point RectStartPoint;
+		private Rectangle Rect = new Rectangle();
+		private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
 
-        // Start Rectangle
-        //
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            // Determine the initial rectangle coordinates...
-            RectStartPoint = e.Location;
-            pictureBox1.Invalidate();
-            pictureBox1.Refresh();
-        }
+		// Start Rectangle
 
-        // Draw Rectangle
-        //
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button != MouseButtons.Left)
-                return;
-            Point tempEndPoint = e.Location;
-            Rect.Location = new Point(
-                Math.Min(RectStartPoint.X, tempEndPoint.X),
-                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
-            Rect.Size = new Size(
-                Math.Abs(RectStartPoint.X - tempEndPoint.X),
-                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
-            pictureBox1.Invalidate();
-            pictureBox1.Refresh();
-        }
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			// Determine the initial rectangle coordinates...
+			RectStartPoint = e.Location;
+			pictureBox1.Invalidate();
+			pictureBox1.Refresh();
+		}
 
-        // Draw Area
-        //
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            // Draw the rectangle...
-            if (pictureBox1.Image != null)
-            {
-                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
-                {
-                    e.Graphics.FillRectangle(selectionBrush, Rect);
-                }
-            }
-        }
+		// Draw Rectangle
+		//
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left)
+			{
+				return;
+			}
 
-        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Right)
-            {
-                if (Rect.Contains(e.Location))
-                {
-                    Debug.WriteLine("Right click");
-                }
-            }
-        }
+			Point tempEndPoint = e.Location;
+			Rect.Location = new Point(Math.Min(RectStartPoint.X, tempEndPoint.X), Math.Min(RectStartPoint.Y, tempEndPoint.Y));
 
-        private void Save_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog save = new SaveFileDialog();
-            save.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;";
+			Rect.Size = new Size(Math.Abs(RectStartPoint.X - tempEndPoint.X), Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
 
-            string pathDefault = Environment.CurrentDirectory;
-            pathDefault = Path.GetFullPath(Path.Combine(pathDefault, @"..\"));
-            if (save.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox1.Image.Save(save.FileName);
+			pictureBox1.Invalidate();
+			pictureBox1.Refresh();
+		}
 
-                MemoryStream ms = new MemoryStream();
-            }
-        }
+		// Draw Area
+		//
+		private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			// Draw the rectangle...
+			if (pictureBox1.Image != null)
+			{
+				for (int i = 0; i < list.Count; i++)
+				{
+					if (listBox1.SelectedItem != null)
+					{
+						int value = (int)listBox1.SelectedItem;
+						if (value == i)
+							e.Graphics.FillRectangle(selectionBrush, list[i]);
+					}
+					e.Graphics.FillRectangle(selectionBrush, list[i]);
+				}
 
-        private void Load_Click(object sender, EventArgs e)
-        {
-            
-            OpenFileDialog open = new OpenFileDialog();
-            open.InitialDirectory = @"C:\Users\s171102\Pictures";
-            open.Filter = "Image files|*.png;*.jpg;*.bmp;";
-            if (open.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox1.Image = (Bitmap)Image.FromFile(open.FileName);
-                //string text = File.ReadAllText(open.FileName);
-            }
-        }
-    }
+				e.Graphics.FillRectangle(selectionBrush, Rect);
+			}
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+				{
+					list.Add(Rect);
+					listBox1.Items.Add(list.Count - 1);
+				}
+				pictureBox1.Invalidate();
+				pictureBox1.Refresh();
+			}
+		}
+
+		private void Save_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog save = new SaveFileDialog();
+			save.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;";
+
+			string pathDefault = Environment.CurrentDirectory;
+			pathDefault = Path.GetFullPath(Path.Combine(pathDefault, @"..\"));
+			if (save.ShowDialog() == DialogResult.OK)
+			{
+				pictureBox1.Image.Save(save.FileName);
+
+				MemoryStream ms = new MemoryStream();
+			}
+		}
+
+		private void Load_Click(object sender, EventArgs e)
+		{
+
+			OpenFileDialog open = new OpenFileDialog();
+			open.InitialDirectory = @"C:\Users\s171102\Pictures";
+			open.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;";
+			if (open.ShowDialog() == DialogResult.OK)
+			{
+				pictureBox1.Image = (Bitmap)Image.FromFile(open.FileName);
+			}
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			pictureBox1.Invalidate();
+			pictureBox1.Refresh();
+		}
+
+		private void Delete_Click(object sender, EventArgs e)
+		{
+			list.Remove(Rect);
+			if(list.Remove(Rect))
+			{
+				listBox1.SelectedItems.Remove(listBox1.SelectedItem);
+				listBox1.Refresh();
+			}
+		}
+	}
 }
