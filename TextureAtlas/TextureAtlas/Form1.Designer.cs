b32e2ca6f673c274a8bee9442ba30771b95d81dd
Alpinski <alpi1997@hotmail.com>
2017-09-03 22:47:20 +1000

    fully done

--- TextureAtlas/TextureAtlas/Form1.Designer.cs
+++ TextureAtlas/TextureAtlas/Form1.Designer.cs
@@ -28,172 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.Play = new System.Windows.Forms.Button();
-			this.Save = new System.Windows.Forms.Button();
-			this.Loader = new System.Windows.Forms.Button();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.groupBox3 = new System.Windows.Forms.GroupBox();
-			this.listBox1 = new System.Windows.Forms.ListBox();
-			this.Delete = new System.Windows.Forms.Button();
-			this.timer1 = new System.Windows.Forms.Timer(this.components);
-			this.button1 = new System.Windows.Forms.Button();
-			this.groupBox1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			this.groupBox2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.groupBox3.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.button1);
-			this.groupBox1.Controls.Add(this.Play);
-			this.groupBox1.Controls.Add(this.Save);
-			this.groupBox1.Controls.Add(this.Loader);
-			this.groupBox1.Controls.Add(this.pictureBox2);
-			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
-			this.groupBox1.Location = new System.Drawing.Point(0, 0);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(223, 593);
-			this.groupBox1.TabIndex = 1;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Functions";
-			// 
-			// Play
-			// 
-			this.Play.Location = new System.Drawing.Point(3, 385);
-			this.Play.Name = "Play";
-			this.Play.Size = new System.Drawing.Size(214, 66);
-			this.Play.TabIndex = 1;
-			this.Play.Text = "Play";
-			this.Play.UseVisualStyleBackColor = true;
-			this.Play.Click += new System.EventHandler(this.Play_Click);
-			// 
-			// Save
-			// 
-			this.Save.AutoSize = true;
-			this.Save.Location = new System.Drawing.Point(3, 457);
-			this.Save.Name = "Save";
-			this.Save.Size = new System.Drawing.Size(101, 133);
-			this.Save.TabIndex = 10;
-			this.Save.Text = "Save";
-			this.Save.UseVisualStyleBackColor = true;
-			this.Save.Click += new System.EventHandler(this.Save_Click);
-			// 
-			// Loader
-			// 
-			this.Loader.AutoSize = true;
-			this.Loader.Location = new System.Drawing.Point(110, 457);
-			this.Loader.Name = "Loader";
-			this.Loader.Size = new System.Drawing.Size(107, 71);
-			this.Loader.TabIndex = 11;
-			this.Loader.Text = "Load";
-			this.Loader.UseVisualStyleBackColor = true;
-			this.Loader.Click += new System.EventHandler(this.Load_Click);
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
-			this.pictureBox2.Location = new System.Drawing.Point(5, 12);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(212, 367);
-			this.pictureBox2.TabIndex = 12;
-			this.pictureBox2.TabStop = false;
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Loader = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Delete = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Stop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Stop);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Play);
+            this.groupBox1.Controls.Add(this.Save);
+            this.groupBox1.Controls.Add(this.Loader);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 593);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Functions";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 53);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Load Boxes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(3, 385);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(214, 26);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Save
+            // 
+            this.Save.AutoSize = true;
+            this.Save.Location = new System.Drawing.Point(3, 457);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(101, 133);
+            this.Save.TabIndex = 10;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Loader
+            // 
+            this.Loader.AutoSize = true;
+            this.Loader.Location = new System.Drawing.Point(110, 457);
+            this.Loader.Name = "Loader";
+            this.Loader.Size = new System.Drawing.Size(107, 71);
+            this.Loader.TabIndex = 11;
+            this.Loader.Text = "Load";
+            this.Loader.UseVisualStyleBackColor = true;
+            this.Loader.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(212, 367);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox2.Controls.Add(this.pictureBox1);
-			this.groupBox2.Location = new System.Drawing.Point(229, 0);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(983, 593);
-			this.groupBox2.TabIndex = 2;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Image";
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-			this.pictureBox1.Location = new System.Drawing.Point(3, 16);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(977, 574);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
-			this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
-			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
-			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-			// 
-			// groupBox3
-			// 
-			this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox3.Controls.Add(this.listBox1);
-			this.groupBox3.Location = new System.Drawing.Point(1215, 0);
-			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(238, 531);
-			this.groupBox3.TabIndex = 3;
-			this.groupBox3.TabStop = false;
-			this.groupBox3.Text = "List";
-			// 
-			// listBox1
-			// 
-			this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(6, 16);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(229, 512);
-			this.listBox1.TabIndex = 0;
-			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-			// 
-			// Delete
-			// 
-			this.Delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.Delete.Location = new System.Drawing.Point(1215, 534);
-			this.Delete.Name = "Delete";
-			this.Delete.Size = new System.Drawing.Size(238, 56);
-			this.Delete.TabIndex = 1;
-			this.Delete.Text = "Delete";
-			this.Delete.UseVisualStyleBackColor = true;
-			this.Delete.Click += new System.EventHandler(this.Delete_Click);
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(110, 534);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(107, 53);
-			this.button1.TabIndex = 13;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ButtonFace;
-			this.ClientSize = new System.Drawing.Size(1452, 593);
-			this.Controls.Add(this.Delete);
-			this.Controls.Add(this.groupBox3);
-			this.Controls.Add(this.groupBox2);
-			this.Controls.Add(this.groupBox1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			this.groupBox2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.groupBox3.ResumeLayout(false);
-			this.ResumeLayout(false);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(229, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(983, 593);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Image";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(977, 574);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(1215, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(238, 531);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "List";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(229, 512);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Delete
+            // 
+            this.Delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Delete.Location = new System.Drawing.Point(1215, 534);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(238, 56);
+            this.Delete.TabIndex = 1;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(3, 417);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(214, 25);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1452, 593);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
@@ -210,6 +223,7 @@
 		private System.Windows.Forms.PictureBox pictureBox2;
 		private System.Windows.Forms.Timer timer1;
 		private System.Windows.Forms.Button button1;
-	}
+        private System.Windows.Forms.Button Stop;
+    }
 }
 
--- TextureAtlas/TextureAtlas/Form1.cs
+++ TextureAtlas/TextureAtlas/Form1.cs
@@ -15,10 +15,14 @@ namespace TextureAtlas
 {
 	public partial class Form1 : Form
 	{
+        /// <summary>
+        /// creates a list of rectangles
+        /// </summary>
 		List<Rectangle> list = new List<Rectangle>();
 
-		//Rectangle mRect;
-
+        /// <summary>
+        /// initialise the everything that needs to be
+        /// </summary>
 		public Form1()
 		{
 			InitializeComponent();
@@ -33,19 +37,24 @@ namespace TextureAtlas
 		private Point RectStartPoint;
 		private Rectangle Rect = new Rectangle();
 		private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
-
-		// Start Rectangle
-
-		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        private int counter = 0;
+        /// <summary>
+        /// what will happen when mouse button is pressed(start drawing rectangle)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
 		{
 			// Determine the initial rectangle coordinates...
 			RectStartPoint = e.Location;
 			pictureBox1.Invalidate();
 			pictureBox1.Refresh();
 		}
-
-		// Draw Rectangle
-		//
+        /// <summary>
+        /// what will happen when mouse button is moved(draw rectangle)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
 		{
 			if (e.Button != MouseButtons.Left)
@@ -62,8 +71,11 @@ namespace TextureAtlas
 			pictureBox1.Refresh();
 		}
 
-		// Draw Area
-		//
+        /// <summary>
+        /// paints a rectangle in the picturebox and puts it into an array
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void pictureBox1_Paint(object sender, PaintEventArgs e)
 		{
 			// Draw the rectangle...
@@ -83,8 +95,12 @@ namespace TextureAtlas
 				e.Graphics.FillRectangle(selectionBrush, Rect);
 			}
 		}
-
-		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        /// <summary>
+        /// when the mouse button is release it will stop drawing a rectangle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+  		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
 		{
 			if (e.Button == MouseButtons.Left)
 			{
@@ -97,7 +113,11 @@ namespace TextureAtlas
 				pictureBox1.Refresh();
 			}
 		}
-
+        /// <summary>
+        /// saves the location of the boxes you drew
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void Save_Click(object sender, EventArgs e)
 		{
 			SaveFileDialog save = new SaveFileDialog();
@@ -118,10 +138,13 @@ namespace TextureAtlas
 				
 			}
 		}
-
+        /// <summary>
+        /// loads your sprite sheet to be previewed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void Load_Click(object sender, EventArgs e)
 		{
-			
 			OpenFileDialog open = new OpenFileDialog();
 			open.InitialDirectory = @"C:\Users\s171102\Pictures";
 			open.Filter = "Image files|*.png;*.jpg;*.bmp;*.tga;";
@@ -134,16 +157,24 @@ namespace TextureAtlas
 				listBox1.Refresh();
 			}
 		}
-
+        /// <summary>
+        /// refreshes the list to keep it updated
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			pictureBox1.Invalidate();
 			pictureBox1.Refresh();
 		}
-
+        /// <summary>
+        /// deletes a rectangle you've drawn and removes that rectangle from list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void Delete_Click(object sender, EventArgs e)
 		{
-			//list.Remove(Rect);
+		
 			list.RemoveAt((int)listBox1.SelectedItem);
 			
 			listBox1.Items.RemoveAt((int)listBox1.SelectedItem);
@@ -153,13 +184,16 @@ namespace TextureAtlas
 			listBox1.Refresh();
 
 		}
-
+        /// <summary>
+        /// these next 2 functions are for dragging and dropping images onto the picturebox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void pictureBox1_DragEnter(object sender, DragEventArgs e)
 		{
 
 			e.Effect = DragDropEffects.Copy;
 		}
-
 		private void pictureBox1_DragDrop(object sender, DragEventArgs e)
 
 		{
@@ -169,7 +203,12 @@ namespace TextureAtlas
 				pictureBox1.Image = img;
 			}
 		}
-		private int counter = 0;
+
+        /// <summary>
+        /// timer that plays the selected parts of image in the second picturebox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		void timer_Tick(object sender, EventArgs e)
 		{
 			pictureBox2.Image = ((Bitmap)pictureBox1.Image).Clone(list[counter], PixelFormat.DontCare);
@@ -183,13 +222,21 @@ namespace TextureAtlas
 			pictureBox2.Refresh();
 			
 		}
-
+        /// <summary>
+        /// plays the selected parts when clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void Play_Click(object sender, EventArgs e)
 		{
 			 pictureBox1.Show();
 			 timer1.Start();
 		}
-
+        /// <summary>
+        /// loads the locations of the boxes when you reopen the tool
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void button1_Click(object sender, EventArgs e)
 		{
 			string line;
@@ -216,5 +263,16 @@ namespace TextureAtlas
 				}
 			}
 		}
-	}
+        /// <summary>
+        /// stops the animation preview in the picturebox2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+            timer1.Stop();
+            pictureBox2.Refresh();
+        }
+    }
 }
